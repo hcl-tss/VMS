3ba0300d1bcf2846301c46f8151a60e9e005223e
vijay <vijay@DESKTOP-QERH5QL>
2019-10-02 11:03:58 +0530

    Login page

--- VMS/App_Start/RouteConfig.cs
+++ VMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace VMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "App", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
---
+++ VMS/Controllers/AppController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMS.Controllers
+{
+    public class AppController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login()
+        {
+            ViewBag.errMsg = "";
+            return View();
+        }
+    }
+}
